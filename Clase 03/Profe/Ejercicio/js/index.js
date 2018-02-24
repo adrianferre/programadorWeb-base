d48907c72c07c0bf5dc53e2a0ed8c35936ea88bb
adrianc4 <adrian_c4@hotmail.com>
2018-02-24 13:04:11 -0300

    Solution 03

---
+++ Clase 03/Profe/Ejercicio/js/index.js
@@ -0,0 +1,42 @@
+// function dayOfTheWeekMessage (dayOfTheWeek) {
+//   switch (dayOfTheWeek) {
+//     case 'Lunes':
+//     case 'lunes':
+//     case 'Martes':
+//     case 'Miércoles':
+//     case 'Miercoles':
+//     case 'Jueves':
+//     case 'Viernes':
+//       console.log('El ' + dayOfTheWeek + ' es día de semana')
+//       break
+//     case 'Sábado':
+//     case 'Sabado':
+//     case 'Domingo':
+//     case 'domingo':
+//     case 'Domingó':
+//       console.log('El ' + dayOfTheWeek + ' es fin de semana')
+//       break
+//     default:
+//       console.log(dayOfTheWeek + ' es un valor invalido')
+//       break
+//   }
+// }
+
+// var day1 = prompt('Ingrese un día')
+// dayOfTheWeekMessage(day1)
+
+// var day2 = prompt('Ingrese un día')
+// dayOfTheWeekMessage(day2)
+
+// var day3 = prompt('Ingrese un día')
+// dayOfTheWeekMessage(day3)
+
+function showConsoleLogWithFirstNameAndLastName () {
+  var firstName = prompt('Ingresa tu nombre')
+  var lastName = prompt('Ingresa tu apellido')
+  console.log('Hola ' + firstName + ' ' + lastName)
+}
+
+showConsoleLogWithFirstNameAndLastName()
+showConsoleLogWithFirstNameAndLastName()
+showConsoleLogWithFirstNameAndLastName()
